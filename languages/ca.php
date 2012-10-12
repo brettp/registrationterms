74967c8938b2edf330ecbf0f8c369f216c95d9b1
Sem <sembrestels@riseup.net>
2012-10-12 18:06:42 +0200

    Added catalan language.

---
+++ languages/ca.php
@@ -0,0 +1,6 @@
+<?php
+$language = array (
+  'registrationterms:agree' => 'He llegit i estic d\'acord amb les <a target="_blank" href="%s">Condicions del Servei</a>',
+  'registrationterms:required' => 'Has d\'estar d\'acord amb les condicions',
+);
+add_translation("ca", $language);
